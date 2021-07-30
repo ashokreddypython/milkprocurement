2447c953f3c37553e8ae73f8b729bfdedbb6ad8f
ashokreddypython <ashokreddypython@gmail.com>
2021-07-30 21:23:23 +0530

    Milk procurement created by Devops Team by Ashok

---
+++ src/test/java/com/rkfreshmilk/milkprocurement/MilkprocurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkfreshmilk.milkprocurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkprocurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
